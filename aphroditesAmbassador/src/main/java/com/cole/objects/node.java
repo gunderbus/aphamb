f627959683574ac7d3d9e9b50992cc59efb05064
gunderbus <cole.gundermann@gmail.com>
2026-03-21 11:14:30 -0400

    dropdowns

--- aphroditesAmbassador/src/main/java/com/cole/objects/node.java
+++ aphroditesAmbassador/src/main/java/com/cole/objects/node.java
@@ -1,6 +1,8 @@
 package com.cole.objects;
 
 
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
@@ -8,14 +10,22 @@ import javafx.scene.layout.Pane;
 public class node {
     private Pane pane;
     private Label name;
+    private ChoiceBox<String> dropDownIn;
+    private ChoiceBox<String> dropDownOut;
 
-    public node(String name1){
+    public node(String name1, String defaultIn, String defaultOut){
         pane = new Pane();
         pane.getStylesheets().add(
             node.class.getResource("/com/cole/styles/node-pane.css").toExternalForm()
         );
         pane.getStyleClass().add("node-pane");
         name = new Label(name1);
+
+        dropDownIn = new ChoiceBox<>();
+        dropDownIn.getItems().addAll(defaultIn);
+
+        dropDownOut = new ChoiceBox<>();
+        dropDownOut.getItems().addAll(defaultOut);
     }
 
     public boolean isClickedLabel(){
